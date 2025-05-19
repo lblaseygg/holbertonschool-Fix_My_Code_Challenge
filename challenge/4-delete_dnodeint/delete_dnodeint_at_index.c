ed4105e4dbf219da370b76804e5904bedd11b009
Luis Feliciano <justblasey@gmail.com>
2025-05-19 14:25:28 -0400

    Adding fixed code

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,13 +11,12 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
+	dlistint_t *current = *head;
 	unsigned int i;
 
 	if (*head == NULL)
 		return (-1);
 
-	current = *head;
 	if (index == 0)
 	{
 		*head = current->next;
